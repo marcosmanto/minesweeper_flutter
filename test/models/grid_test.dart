1bffe3fda2e512029b9b7eedf408206d33d8c0f9
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-17 11:56:25 -0300

    Added test for Grid model class.

---
+++ test/models/grid_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:minesweeper_flutter/models/grid.dart';
+
+void main() {
+  group(
+    'Grid test',
+    () {
+      test(
+        'Win the game',
+        () {
+          Grid grid = Grid(
+            rows: 2,
+            columns: 2,
+            bombs: 0,
+          );
+
+          grid.cells[0].mineCell();
+          grid.cells[3].mineCell();
+
+          grid.cells[0].toogleMarked();
+          grid.cells[1].open();
+          grid.cells[2].open();
+          grid.cells[3].toogleMarked();
+
+          expect(grid.resolved, isTrue);
+        },
+      );
+    },
+  );
+}
