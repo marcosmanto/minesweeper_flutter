8ed89eb647823cb6041e8f322f1b2424ea804d58
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-17 17:41:49 -0300

    finished app. Added dynamic grid based on device dimensions and random number of bombs.

--- lib/screens/minesweeper.dart
+++ lib/screens/minesweeper.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: avoid_print
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:minesweeper_flutter/components/grid_widget.dart';
 import 'package:minesweeper_flutter/components/result_widget.dart';
@@ -16,43 +18,82 @@ class Minesweeper extends StatefulWidget {
 
 class _MinesweeperState extends State<Minesweeper> {
   bool? _win;
-  final Grid _grid = Grid(rows: 12, columns: 12, bombs: 3);
+  Grid? _grid;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  int randBetween(int min, int max) => min + Random().nextInt((max + 1) - min);
+
+  int randomNumberOfBombs(int rows, int columns) {
+    int total = columns * rows;
+    int max = (0.2 * total).floor();
+    int min = (0.1 * total).floor();
+    return randBetween(min, max);
+  }
+
+  Grid _getGrid(double width, double height) {
+    if (_grid == null) {
+      int qtyColumns = 15;
+      double sizeOfCell = width / qtyColumns;
+      int qtyRows = (height / sizeOfCell).floor();
+
+      _grid = Grid(
+        rows: qtyRows,
+        columns: qtyColumns,
+        bombs: randomNumberOfBombs(qtyRows, qtyColumns),
+      );
+    }
+    return _grid!;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: ResultWidget(win: _win, onRestarted: _restart),
-        body: GridWidget(
-          grid: _grid,
-          onOpen: _open,
-          onToggleMarked: _toggleMarked,
+        body: Container(
+          color: Colors.grey,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return GridWidget(
+                grid: _getGrid(
+                  constraints.maxWidth,
+                  constraints.maxHeight,
+                ),
+                onOpen: _open,
+                onToggleMarked: _toggleMarked,
+              );
+            },
+          ),
         ),
       ),
     );
   }
 
   void _open(Cell cell) {
-    if (_win != null) return;
+    if (_win != null || _grid == null) return;
     setState(() {
       try {
         cell.open();
-        if (_grid.resolved) {
+        if (_grid!.resolved) {
           _win = true;
         }
       } on ExplosionException {
         _win = false;
-        _grid.revealBombs();
+        _grid!.revealBombs();
       }
     });
     print('Opened cell [${cell.row},${cell.column}]');
   }
 
   void _toggleMarked(Cell cell) {
-    if (_win != null) return;
+    if (_win != null || _grid == null) return;
     setState(() {
       cell.toggleMarked();
-      if (_grid.resolved) {
+      if (_grid!.resolved) {
         _win = true;
       }
     });
@@ -60,10 +101,11 @@ class _MinesweeperState extends State<Minesweeper> {
   }
 
   void _restart() {
+    if (_grid == null) return;
     print('Restarting...');
     setState(() {
       _win = null;
-      _grid.restart();
+      _grid!.restart();
     });
   }
 }
